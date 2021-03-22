2705119261857005c16b6eacd9409e2c9b99d798
Sara Bonati <52385894+SaraBonati@users.noreply.github.com>
2021-03-22 15:54:43 +0100

    Add files via upload

---
+++ Scripts/prepare_train_data.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+This script prepares the data for the online sentence similarity survey 
+done as a seminar project (Natural Language Processing WS 2020/2021) @ 
+FU Berlin. We use phrases from the STS dataset: sentence pairs whose 
+length (in number of words) exceeds a threshold $l$ are excluded. Here we 
+set $l=35$.
+The 
+
+@authors: Sara Bonati, Irina Kokoshko
+"""
+
+# general utility import
+import numpy as np
+import os
+import pandas as pd
+import csv
+import nltk
+import pickle
+
+# directory management
+wdir        = os.getcwd()
+
+# import data (long sentences)
+sentences_long   = pd.read_csv(os.path.join(wdir,'STS.input.MSRpar.txt'),
+                          sep=r'\t',
+                          names=["SentenceA","SentenceB"],
+                          header=None,
+                          engine='python')
+gs_long          = pd.read_csv(os.path.join(wdir,'STS.gs.MSRpar.txt'),
+                               header=None,
+                               names=['Score'],
+                               engine='python')
+data_long        = pd.concat([sentences_long, gs_long], axis=1, sort=False)
+
+# import data (short sentences)
+sentences_short  = pd.read_csv(os.path.join(wdir,'STS.input.MSRvid.txt'),
+                               sep=r'\t',
+                               names=["SentenceA","SentenceB"],
+                               header=None,
+                               engine='python')
+gs_short         = pd.read_csv(os.path.join(wdir,'STS.gs.MSRvid.txt'),
+                               header=None,
+                               names=['Score'],
+                               engine='python')
+data_short       = pd.concat([sentences_short, gs_short], axis=1, sort=False)
+
+# merge data in 1 dataframe
+data             = pd.concat([data_long, data_short],
+                             axis=0,
+                             sort=False,
+                             ignore_index=True)
+
+# LENGTH CHECK
+# ----------------------------------------------------------------------------
+# We consider max_length to be the number of words in a sentence!
+max_length         = 35
+
+sent_listA = data['SentenceA'].to_list()
+sent_listA = np.array([s if len(nltk.word_tokenize(s)) < max_length else None for s in sent_listA])
+to_keep    = np.where(sent_listA!=None)
+data       = data.iloc[to_keep[0],:]
+
+file_name  = 'sentences.pkl'
+data.to_pickle(file_name)
